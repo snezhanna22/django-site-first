bff375dd77b8b0d7f84910dbd8abbdea7a961f48
Snezhanna <snezhanna022@gmail.com>
2024-05-26 01:02:49 +0300

    after adding fixtures for goods

--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
+
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title' : 'Home - Главная',
         'content' : 'Магазин мебели HOME',
+        'categories' : categories,
     }
 
     return render(request, 'main/index.html', context)

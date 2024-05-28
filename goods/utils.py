d99625eff380337d2e1a3e29ba7c373d1409cc5d
Snezhanna <snezhanna022@gmail.com>
2024-05-28 22:21:42 +0300

    full text search well-done!

--- goods/utils.py
+++ goods/utils.py
@@ -1,23 +1,47 @@
 from goods.models import Products
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 
-#функция поиск по сайту
+# функция поиск по сайту
 def q_search(query):
+
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    
-    vector = SearchVector('name', 'description')
+
+    vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
+    resalt = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
+    result = resalt.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="backround-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = resalt.annotate(
+        headline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="backround-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
 
+    return resalt
 
-
-    
     # keywords = [word for word in query.split() if len(word) > 2]
 
     # q_objects = Q()

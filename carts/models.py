cec68af71832160935be974c1ade52720ca1c8f9
Snezhanna <snezhanna022@gmail.com>
2024-06-02 14:25:24 +0300

    add-to-cart ajax request and json response

--- carts/models.py
+++ carts/models.py
@@ -23,6 +23,7 @@ class Cart(models.Model):
         db_table = 'cart'
         verbose_name = 'Корзина'
         verbose_name_plural = 'Корзина'
+        ordering = ('id',)
 
     objects = CartQueryset().as_manager()
 
--- carts/templatetags/carts_tags.py
+++ carts/templatetags/carts_tags.py
@@ -1,10 +1,10 @@
 from atexit import register
 from django import template
+from carts.utils import get_user_carts
 from carts.models import Cart
 
 register = template.Library()
 
 @register.simple_tag()
 def user_carts(request):
-    if request.user.is_authenticated:
-        return Cart.objects.filter(user=request.user)
+    return get_user_carts(request)
